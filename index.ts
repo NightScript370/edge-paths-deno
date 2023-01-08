1d339333160b9a31796c53c2ac8e8553ad37cbe8
NightScript <18664762+NightScript370@users.noreply.github.com>
2023-01-08 14:58:30 -0500

    Deno-ify

--- index.ts
+++ index.ts
@@ -1,18 +1,16 @@
-import { existsSync } from "fs"
-import path from "path"
-import which from "which"
+import { join } from "https://deno.land/std@0.171.0/path/mod.ts"
 
-let platform = process.platform
+let platform = Deno.build
 
 // Caller will ensure that the platform is linux, otherwise null will be returned
-function getEdgeLinux(
+async function getEdgeLinux(
 	name:
 		| "microsoft-edge-dev"
 		| "microsoft-edge-beta"
 		| "microsoft-edge-stable",
 ): string | null {
 	try {
-		let path = which.sync(name)
+		let path = await new Command('which', { args: [name] }).output()
 		return path
 	} catch (e) {}
 
@@ -21,21 +19,21 @@ function getEdgeLinux(
 
 
 // Caller will ensure that the platform is windows, otherwise null will be returned
-function getEdgeWindows(
+async function getEdgeWindows(
 	edgeDirName: "Edge" | "Edge Dev" | "Edge Beta" | "Edge SxS",
 ): string | null {
 	let paths = []
 	let suffix = `\\Microsoft\\${edgeDirName}\\Application\\msedge.exe`
 	let prefixes = [
-		process.env.LOCALAPPDATA,
-		process.env.PROGRAMFILES,
-		process.env["PROGRAMFILES(X86)"],
-	].filter((v) => !!v)
+		'LOCALAPPDATA',
+		'PROGRAMFILES',
+		"PROGRAMFILES(X86)",
+	].map(string => Deno.env.get(string)).filter(Boolean)
 
 	for (let prefix of prefixes) {
-		let edgePath = path.join(prefix!, suffix)
+		let edgePath = join(prefix!, suffix)
 		paths.push(edgePath)
-		if (existsSync(edgePath)) {
+		if (async exists(edgePath)) {
 			return edgePath
 		}
 	}
@@ -44,8 +42,8 @@ function getEdgeWindows(
 }
 
 // Caller will ensure that the platform is macos, otherwise null will be returned
-function getEdgeDarwin(defaultPath: string): string | null {
-	if (existsSync(defaultPath)) {
+async function getEdgeDarwin(defaultPath: string): string | null {
+	if (await exists(defaultPath)) {
 		return defaultPath
 	}
 
@@ -54,45 +52,45 @@ function getEdgeDarwin(defaultPath: string): string | null {
 
 const edgePaths = {
 	edge: {
-		linux: () => getEdgeLinux("microsoft-edge-stable"),
-		darwin: () =>
-			getEdgeDarwin(
+		linux: async () => await getEdgeLinux("microsoft-edge-stable"),
+		darwin: async () =>
+			await getEdgeDarwin(
 				"/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge",
 			),
-		win32: () => getEdgeWindows("Edge"),
+		windows: async () => await getEdgeWindows("Edge"),
 	},
 	dev: {
-		linux: () => getEdgeLinux("microsoft-edge-dev"),
-		darwin: () =>
-			getEdgeDarwin(
+		linux: async () => await getEdgeLinux("microsoft-edge-dev"),
+		darwin: async () =>
+			await getEdgeDarwin(
 				"/Applications/Microsoft Edge Dev.app/Contents/MacOS/Microsoft Edge Dev",
 			),
-		win32: () => getEdgeWindows("Edge Dev"),
+		windows: async () => await getEdgeWindows("Edge Dev"),
 	},
 	beta: {
-		linux: () => getEdgeLinux("microsoft-edge-beta"),
-		darwin: () =>
-			getEdgeDarwin(
+		linux: async () => await getEdgeLinux("microsoft-edge-beta"),
+		darwin: async () =>
+			await getEdgeDarwin(
 				"/Applications/Microsoft Edge Beta.app/Contents/MacOS/Microsoft Edge Beta",
 			),
-		win32: () => getEdgeWindows("Edge Beta"),
+		windows: async () => await getEdgeWindows("Edge Beta"),
 	},
 	canary: {
 		// linux: getEdgeLinux("microsoft-edge-beta"),
-		darwin: () =>
-			getEdgeDarwin(
+		darwin: async () =>
+			await getEdgeDarwin(
 				"/Applications/Microsoft Edge Canary.app/Contents/MacOS/Microsoft Edge Canary",
 			),
-		win32: () => getEdgeWindows("Edge SxS"),
+		windows: async () => await getEdgeWindows("Edge SxS"),
 	},
 }
 
 // returns edge path
-export function getEdgePath(): string {
+export async function getEdgePath(): string {
 	let edge = edgePaths.edge
 
 	if (platform && platform in edgePaths.edge) {
-		let pth = edge[platform as keyof typeof edge]()
+		let pth = await edge[platform as keyof typeof edge]()
 		if (pth) {
 			return pth
 		}
@@ -101,11 +99,11 @@ export function getEdgePath(): string {
 }
 
 // Returns edge dev path
-export function getEdgeDevPath(): string {
+export async function getEdgeDevPath(): string {
 	let edgeDev = edgePaths.dev
 
 	if (platform && platform in edgeDev) {
-		let pth = edgeDev[platform as keyof typeof edgeDev]()
+		let pth = await edgeDev[platform as keyof typeof edgeDev]()
 		if (pth) {
 			return pth
 		}
@@ -114,11 +112,11 @@ export function getEdgeDevPath(): string {
 }
 
 // Returns edge beta path if it is available
-export function getEdgeBetaPath(): string {
+export async function getEdgeBetaPath(): string {
 	let edgeBeta = edgePaths.beta
 
 	if (platform && platform in edgeBeta) {
-		let pth = edgeBeta[platform as keyof typeof edgeBeta]()
+		let pth = await edgeBeta[platform as keyof typeof edgeBeta]()
 		if (pth) {
 			return pth
 		}
@@ -127,11 +125,11 @@ export function getEdgeBetaPath(): string {
 }
 
 // Returns edge canary paths.
-export function getEdgeCanaryPath(): string {
+export async function getEdgeCanaryPath(): string {
 	let edgeCanary = edgePaths.canary
 
 	if (platform && platform in edgeCanary) {
-		let pth = edgeCanary[platform as keyof typeof edgeCanary]()
+		let pth = await edgeCanary[platform as keyof typeof edgeCanary]()
 		if (pth) {
 			return pth
 		}
@@ -140,27 +138,27 @@ export function getEdgeCanaryPath(): string {
 }
 
 // This will try to get any edge from bleeding edge to most stable version
-export function getAnyEdgeLatest(): string {
+export async function getAnyEdgeLatest(): string {
 	try {
-		return getEdgeCanaryPath()
+		return await getEdgeCanaryPath()
 	} catch (e) {
 		throwIfNotEdgePathIssue(e)
 	}
 
 	try {
-		return getEdgeDevPath()
+		return await getEdgeDevPath()
 	} catch (e) {
 		throwIfNotEdgePathIssue(e)
 	}
 
 	try {
-		return getEdgeBetaPath()
+		return await getEdgeBetaPath()
 	} catch (e) {
 		throwIfNotEdgePathIssue(e)
 	}
 
 	try {
-		return getEdgePath()
+		return await getEdgePath()
 	} catch (e) {
 		throwIfNotEdgePathIssue(e)
 	}
@@ -173,27 +171,27 @@ export function getAnyEdgeLatest(): string {
 
 // This will try to get edge from stable version to bleeding version
 // Useful for playwright, puppeteer related stuff
-export function getAnyEdgeStable(): string {
+export async function getAnyEdgeStable(): string {
 	try {
-		return getEdgePath()
+		return await getEdgePath()
 	} catch (e) {
 		throwIfNotEdgePathIssue(e)
 	}
 
 	try {
-		return getEdgeBetaPath()
+		return await getEdgeBetaPath()
 	} catch (e) {
 		throwIfNotEdgePathIssue(e)
 	}
 
 	try {
-		return getEdgeDevPath()
+		return await getEdgeDevPath()
 	} catch (e) {
 		throwIfNotEdgePathIssue(e)
 	}
 
 	try {
-		return getEdgeCanaryPath()
+		return await getEdgeCanaryPath()
 	} catch (e) {
 		throwIfNotEdgePathIssue(e)
 	}
@@ -228,4 +226,20 @@ function throwIfNotEdgePathIssue(obj: any) {
 	}
 
 	throw obj
+}
+
+async function exists (filename: string): Promise<boolean> {
+	try {
+		await Deno.stat(filename);
+		// successful, file or directory must exist
+		return true;
+	} catch (error) {
+		if (error instanceof Deno.errors.NotFound) {
+			// file or directory does not exist
+			return false;
+		} else {
+			// unexpected error, maybe permissions, pass it along
+			throw error;
+		}
+	}
 }
